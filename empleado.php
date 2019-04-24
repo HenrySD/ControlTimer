137120721b41d7a754f2fd87ee3b20254db413ac
Henry Lopez <henryderaslopez@gmail.com>
2019-04-23 23:10:10 -0500

    olis

--- empleado.php
+++ empleado.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,26 +8,97 @@
     <title>Vista Empleado</title>
 
 
-		<!--//////////////////////////////-->
-            <!--CDN bootstrap y css-->
-        <!--//////////////////////////////-->
+    <!--//////////////////////////////-->
+    <!--CDN bootstrap y css-->
+    <!--//////////////////////////////-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="carpetas/css/estilo.css">
 
 
-    			<!--**********************CDN de jQuery y js*******************************-->
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <!--**********************CDN de jQuery y js*******************************-->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js">
-</script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js">
-</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js">
+    </script>
 
 </head>
-<body class="cuerpo-ve">
-    
-<div class="contenedor-ve ">
 
-</div>
+<body class="cuerpo-ve">
+
+    <div class="contenedor-ve ">
+        <div class="container">
+            <div class="card ">
+                <div class="card-header">
+                    Control de Asistencia
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-3 bg-info ml-2">
+                            <p>Lunes 22 de Abril del 2019 </p>
+                        </div>
+                        <div class="col-md-3 bg-danger ml-5">
+                            <p> 10:34:40 PM </p>
+                        </div>
+                        <div class="col-md-3 ml-5">
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 bg-info ml-2 mt-3" style="width:200px; height:250px;">
+
+                        </div>
+
+                        <div class="col-md-8  ml-2 mt-3" style="width:100px; height:250px;">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>Larry</td>
+                                        <td>the Bird</td>
+                                        <td>@twitter</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>Larry</td>
+                                        <td>the Bird</td>
+                                        <td>@twitter</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 bg-info ml-2 mt-3" style="height:50px;">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
