1eb8f5fbff9364d91e923e4df682c2b870fe9ca4
vicki <estefanya_rami1997@hotmail.com>
2019-06-05 16:51:11 -0600

    hoy si

--- asistencia.php
+++ asistencia.php
@@ -1,6 +1,20 @@
 <?php
 require("partials/cabeza.php");
 ?>
+ <!-- este es el modal de ayuda-->
+ <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content ">
+            <div class="embed-responsive embed-responsive-4by3">
+                <iframe class="embed-responsive-item"
+                src="https://www.youtube.com/embed/xuguXfAITe4?autoplay=0&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="container ">
     <div class="row">
         <div class="col-sm-11">
